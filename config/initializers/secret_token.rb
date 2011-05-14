2837923df8f95f05c39a31e769932ae4cebcc90c
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2011-05-14 16:53:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Htmlnote::Application.config.secret_token = '1cf5ebd212e86c73f92b81bcce27cec474f64d83a719f288cea64e37f313fb6deb2256ebecb08b65a5af8f956d5f766287f3a9ff5307b60733d0e2de54167b1c'
